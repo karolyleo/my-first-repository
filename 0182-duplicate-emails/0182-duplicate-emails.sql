865a7b32ee299d6a3cb60be888ef94b8bf5c3593
Leonard Karoly <118474999+karolyleo@users.noreply.github.com>
2023-09-21 15:08:33 -0500

    Time: 586 ms (99.39%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 SELECT p.email as Email 
 FROM Person p
-WHERE (SELECT COUNT(a.email) FROM Person a WHERE a.email = p.email) > 1
 GROUP BY email 
+HAVING COUNT(email) > 1; 
